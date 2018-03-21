ccccc321c9c3541fb1b26c047f80e50eb134e009
InternetWarrior <liameverton12@gmail.com>
2018-03-21 10:55:02 +0000

    Created Main, to handle the entry point of the Java Application.

---
+++ src/puzzle/Main.java
@@ -0,0 +1,20 @@
+package puzzle;
+
+import puzzle.frame.PuzzleFrame;
+
+/**
+ * Created by Liam on 3/21/2018.
+ */
+public class Main {
+
+	public static final String IMAGE_DIRECTOY = "resources/";
+
+	public static final String IMAGE_PREFIX = "bart";
+
+	public static final String IMAGE_TYPE = ".jpg";
+
+	public static void main(String[] args) {
+		new PuzzleFrame("Puzzle Game");
+	}
+
+}
