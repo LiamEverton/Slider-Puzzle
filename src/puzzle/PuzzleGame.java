3d3074ff60b045468863b4e523c0b89348355c1c
InternetWarrior <liameverton12@gmail.com>
2018-03-21 10:55:42 +0000

    Created PuzzleGame, which encapsulates the logic of the puzzle game.

---
+++ src/puzzle/PuzzleGame.java
@@ -0,0 +1,16 @@
+package puzzle;
+
+import puzzle.frame.PuzzleFrame;
+
+/**
+ * Created by Liam on 3/21/2018.
+ */
+public class PuzzleGame {
+
+	private PuzzleFrame puzzleFrame;
+
+	public PuzzleGame() {
+		this.puzzleFrame = new PuzzleFrame("Puzzle Game");
+	}
+
+}
